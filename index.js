b214d14fb42ead0307a82c72831003937c8c5402
romantrickey-ops <roman.trickey@students.msswarriors.org>
2026-04-17 14:26:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let firstName = 'Brenda';
 let jobTitle = 'office worker';
 let city = 'new york';
-let newjobPost = 'hello ${brenda},
+let newjobPost = 'hello ${firstName},
   ${'office worker'}
 ${'new york'}
 document.getElementById("result").innerHTML = newjobPost;
