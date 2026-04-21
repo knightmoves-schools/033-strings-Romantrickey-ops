b46f7e8344b9ab17bb71e12baafe9571d885d8c5
romantrickey-ops <roman.trickey@students.msswarriors.org>
2026-04-21 14:08:38 -0500

    try this

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let firstName = 'Brenda';
 let jobTitle = 'office worker';
 let city = 'new york';
-let newjobPost = 'hello ${firstName},
-  ${'office worker'}
-${'new york'}
+let newjobPost = `hello ${firstName},
+Thank you for your interest in ${'office worker'} in ${city}. 
+Someone will reach out to you shortly.
+Sincerely,
+Roman`
+
 document.getElementById("result").innerHTML = newjobPost;
